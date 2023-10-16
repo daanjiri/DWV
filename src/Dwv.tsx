a056e9cb61ab8c34662a2d5ea3249dd666af31a1
daanjiri <da.jimenez166@gmail.com>
2023-10-15 20:26:05 -0700

    fix showing image

--- src/Dwv.tsx
+++ src/Dwv.tsx
@@ -1,49 +1,34 @@
-import { App } from "dwv";
+import { App,ViewConfig,AppOptions} from "dwv";
 import { useEffect, useRef } from "react";
+import stackUrls from "./dummydata"
 
 const Dwv: React.FC = () => {
-  const viewerRef = useRef<HTMLDivElement | null>(null);
+  const viewerRef = useRef<any>(null);
 
   useEffect(() => {
-    // Initialize the DWV application
     const app = new App();
-
-    // If 'App' method 'init' has known parameters, specify them. Otherwise, you might use any.
-    const appOptions: any = {
-        containerDivId: "dwv",
-        tools: ["Scroll", "ZoomAndPan"],
-        isMobile: false
-        };
-
-          
-    app.init(appOptions);
-
-    // Sample DICOM stack URLs. Replace these with the paths to your DICOM files.
-    const stackUrls = [
-      "path/to/slice1.dcm",
-      "path/to/slice2.dcm",
-      "path/to/slice3.dcm"
-      // Add more slices as needed
-    ];
-
+    // initialise
+    const viewConfig0 = new ViewConfig('layerGroup0');
+    const viewConfigs = {'*': [viewConfig0]};
+    const options = new AppOptions(viewConfigs);
+    app.init(options);
+    //load urls
     app.loadURLs(stackUrls);
 
-    // Cleanup on component unmount
-    // return () => app.destroy();
+    return () => app.reset();
+
   }, []);
 
   return (
-    <div
-      ref={viewerRef}
-      id="dwv"
-      style={{ width: "512px", height: "512px", border: "1px solid red" }}
+    <div  ref={viewerRef}>
+      <div
+
+      id="layerGroup0"
+      style={{ width: "600px", height: "600px", border: "1px solid red" }}
     >
-      <div className="layerContainer">
-        <canvas className="imageLayer">
-          Only for HTML5 compatible browsers...
-        </canvas>
-      </div>
     </div>
+    </div>
+    
   );
 };
 
---
+++ src/dummydata.tsx
@@ -0,0 +1,7 @@
+   // Sample DICOM stack URLs. Replace these with the paths to your DICOM files.
+   const stackUrls = [
+    'https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323851.dcm',
+    // 'https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323707.dcm',
+    // 'https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323563.dcm'
+    ];
+  export default stackUrls
