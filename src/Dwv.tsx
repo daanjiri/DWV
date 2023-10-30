64bd50193e225e6ab7c0dc59bc27eb19fd45ae72
daanjiri <da.jimenez166@gmail.com>
2023-10-29 20:00:30 -0700

    add material

--- src/Dwv.tsx
+++ src/Dwv.tsx
@@ -1,6 +1,6 @@
-import { App,ViewConfig,AppOptions, ToolConfig} from "dwv";
+import { App, ViewConfig, AppOptions, ToolConfig } from "dwv";
 import { useEffect, useRef } from "react";
-import stackUrls from "./dummydata"
+import stackUrls from "./dummydata";
 
 const Dwv: React.FC = () => {
   const viewerRef = useRef<any>(null);
@@ -8,37 +8,33 @@ const Dwv: React.FC = () => {
   useEffect(() => {
     const app = new App();
     // initialise
-    const viewConfig0 = new ViewConfig('layerGroup0');
-    const viewConfigs = {'*': [viewConfig0]};
+    const viewConfig0 = new ViewConfig("layerGroup0");
+    const viewConfigs = { "*": [viewConfig0] };
     const options = new AppOptions(viewConfigs);
     options.tools = {
       ZoomAndPan: new ToolConfig(),
-      Scroll: new ToolConfig()
+      Scroll: new ToolConfig(),
     };
 
     app.init(options);
     //load urls
     app.loadURLs(stackUrls);
 
-    app.addEventListener('load', function () {
-      app.setTool('ZoomAndPan');
-      app.setTool('Scroll');
+    app.addEventListener("load", function () {
+      app.setTool("ZoomAndPan");
+      app.setTool("Scroll");
     });
 
     return () => app.reset();
-
   }, []);
 
   return (
-    <div  ref={viewerRef}>
+    <div ref={viewerRef}>
       <div
-
-      id="layerGroup0"
-      style={{ width: "600px", height: "600px", border: "1px solid red" }}
-    >
+        id="layerGroup0"
+        style={{ width: "600px", height: "600px", border: "1px solid red" }}
+      ></div>
     </div>
-    </div>
-    
   );
 };
 
